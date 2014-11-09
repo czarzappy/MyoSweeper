1de0de0ab418565faee465285919b37bca6bc529
jeanlucchamaa <jlchamaa@gmail.com>
2014-11-09 00:15:17 -0800

    all necessary com files: need to edit inputs

---
+++ keylog/test_1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			if (Input.GetKeyDown(KeyCode.A)==true) {
+						Debug.Log ("Success!!!");
+				}
+		}
+	}
